76e472836f6888ee97d92a010ae604a6de56fd65
Dayitva Goel <dayitvagoel@gmail.com>
2024-10-24 17:29:27 +0530

    Update lero-landing-page.tsx

--- components/lero-landing-page.tsx
+++ components/lero-landing-page.tsx
@@ -78,7 +78,7 @@ export function LeroLandingPageComponent() {
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Lero&quot;s Vision</h2>
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Lero Vision</h2>
                 <p className="max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
                   Lero is the go-to platform for decentralized finance investors, offering a comprehensive
                   suite of tools to optimize returns and manage risk across all chains and assets for all staking and
